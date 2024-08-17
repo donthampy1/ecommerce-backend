21f12dbfd89ccbe51a5391a563b709a8ec7c892a
donthampy1 <150425930+donthampy1@users.noreply.github.com>
2024-08-17 07:06:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.use('/productsearch',productsearch.router)
 
 
 
-app.listen(3000,()=>{
-    console.log('listening in port 3000')
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
